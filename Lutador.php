2bc2ffb0cdf90d83cb86072df57b4f28b3b5b022
wesleyaleixo <contato@wesleyaleixo.com>
2016-12-01 15:17:55 -0200

    Classe Lutador.

---
+++ Lutador.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ *
+ * @author Wesley Aleixo
+ */
+class Lutador {
+    
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+
+    function __construct($no, $na, $id, $al, $pe, $vi, $de, $em) {
+        
+        $this->nome = $no;
+        $this->nacionalidade = $na;
+        $this->idade = $id;
+        $this->altura = $al;
+        $this->peso = $pe;
+        $this->vitorias = $vi;
+        $this->derrotas = $de;
+        $this->empates = $em;
+    }
+
+    
+    public function apresentar(){
+        
+    }
+    
+    public function status(){
+        
+    }
+    
+    public function ganhaLuta(){
+        
+    }
+    
+    public function perdeLuta(){
+        
+    }
+    
+    public function empatarLuta(){
+        
+    }
+            
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getNacionalidade() {
+        return $this->nacionalidade;
+    }
+
+    function getIdade() {
+        return $this->idade;
+    }
+
+    function getAltura() {
+        return $this->altura;
+    }
+
+    function getPeso() {
+        return $this->peso;
+    }
+
+    function getCategoria() {
+        return $this->categoria;
+    }
+
+    function getVitorias() {
+        return $this->vitorias;
+    }
+
+    function getDerrotas() {
+        return $this->derrotas;
+    }
+
+    function getEmpates() {
+        return $this->empates;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setNacionalidade($nacionalidade) {
+        $this->nacionalidade = $nacionalidade;
+    }
+
+    function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+    function setVitorias($vitorias) {
+        $this->vitorias = $vitorias;
+    }
+
+    function setDerrotas($derrotas) {
+        $this->derrotas = $derrotas;
+    }
+
+    function setEmpates($empates) {
+        $this->empates = $empates;
+    }
+}
