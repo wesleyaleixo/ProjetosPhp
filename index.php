7a622a21737e86b040dfe8a4836734887da35500
wesleyaleixo <contato@wesleyaleixo.com>
2016-12-02 11:10:02 -0200

    Atualização da classe.
    http://www.cursoemvideo.com

--- index.php
+++ index.php
@@ -7,15 +7,22 @@
     <body>
         <?php
             require_once 'Lutador.php';
+            require_once 'Luta.php';
             
             $l = array();
             
             $l[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 68.9, 11, 2, 1);
             $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
             
-            $l[0]->perdeLuta();
-            $l[0]->apresentar();
-            $l[0]->status();            
+            $UEC01 = new Luta();
+            $UEC01->marcarLuta($l[0], $l[1]);
+            $UEC01->lutar();
+            $l[0]->status();
+            $l[1]->status();
+            
+            //$l[0]->perdeLuta();
+            //$l[0]->apresentar();
+            //$l[0]->status();            
         ?>
     </body>
 </html>
